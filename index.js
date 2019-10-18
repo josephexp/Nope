4adad2b804e9943bb1babf50c982da7656e78389
josephexp <joseph.antony@experionglobal.com>
2019-10-18 12:25:32 +0530

    fix

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ app.get('/', (req, res) => {
 }); //route handler
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, "127.0.0.1", () => {
-  console.log('Example app listening on port 8000!')
+app.listen(PORT, () => {
+  console.log('ha')
 });
