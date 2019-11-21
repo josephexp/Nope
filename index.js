0c978ddb01f3c535f716bb15b73d994d9a9dd6ec
josephexp <joseph.antony@experionglobal.com>
2019-11-21 17:43:25 +0530

    Auth flow finished

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
-const express = require('express')
+const express = require("express");
+const mongoose = require("mongoose");
+const keys = require("./config/keys");
+const cookieSession = require("cookie-session");
+const passport = require("passport");
+require("./models/User");
+require("./services/passport");
+
+mongoose.connect(keys.mongoURI);
+
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-}); //route handler
+// middlewares (cookieSessionMiddleware, passportMiddleware)
+// used to wrap multiple routeHandlers with some logic
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
+    keys: [keys.cookieKey]
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
+require("./routes/authRoutes")(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log('ha')
+  console.log("ha");
 });
