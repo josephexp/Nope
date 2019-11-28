5f917be5be404b88a93134f3f3fdd3ed0c668144
josephexp <joseph.antony@experionglobal.com>
2019-11-28 13:21:03 +0530

    react-client-auth-done

---
+++ client/src/components/Landing.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Landing = () => {
+  return (
+    <div style={{ textAlign: "center" }}>
+      <h1>Nope</h1>
+      Collect
+    </div>
+  );
+};
+
+export default Landing;
--- services/passport.js
+++ services/passport.js
@@ -22,17 +22,15 @@ passport.use(
       callbackURL: "/auth/google/callback",
       proxy: true
     },
-    (accessToken, refreshToken, profile, done) => {
-      User.findOne({ googleId: profile.id }).then(existingUser => {
-        if (existingUser) {
-          console.log(accessToken, refreshToken, profile, done);
-          done(null, existingUser);
-        } else {
-          new User({ googleId: profile.id })
-            .save()
-            .then(user => done(null, user));
-        }
-      });
+    async (accessToken, refreshToken, profile, done) => {
+      const existingUser = await User.findOne({ googleId: profile.id });
+      
+      if (existingUser) {
+        // console.log(accessToken, refreshToken, profile, done);
+        return done(null, existingUser);
+      }
+      const user = await new User({ googleId: profile.id }).save();
+      done(null, user);
     }
   )
 );
