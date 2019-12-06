39a931e5aa3bfd12d06704881db6c5f6b1bfac77
josephexp <joseph.antony@experionglobal.com>
2019-12-06 12:24:52 +0530

    billing added

--- routes/billingRoutes.js
+++ routes/billingRoutes.js
@@ -1,8 +1,8 @@
 const keys = require("../config/keys");
 const stripe = require("stripe")(keys.stripeSecretKey);
-
+const requireLogin = require("../middlewares/requireLogin");
 module.exports = app => {
-  app.post("/api/stripe", async (req, res) => {
+  app.post("/api/stripe", requireLogin, async (req, res) => {
     // reachout to stripe api finilize the charge
     const charge = await stripe.charges.create({
       amount: 500,
@@ -10,6 +10,10 @@ module.exports = app => {
       source: req.body.id,
       description: "5$ for 5 credits"
     });
-    console.log(charge);
+
+    req.user.credits += 5;
+    const user = await req.user.save();
+
+    res.send(user);
   });
 };
