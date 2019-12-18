7f63c19a88e2be8826faca5cf40828291c4a8023
josephexp <joseph.antony@experionglobal.com>
2019-12-18 19:34:12 +0530

    survey completed

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,3 +1,6 @@
+const _ = require("lodash");
+const { Path } = require("path-parser");
+const { URL } = require("url");
 const mongoose = require("mongoose");
 const requireLogin = require("../middlewares/requireLogin");
 const requireCredits = require("../middlewares/requireCredits");
@@ -34,7 +37,41 @@ module.exports = app => {
     }
   });
 
-  app.get("/api/surveys/thanks", (req, res) => {
+  app.get("/api/surveys/:surveyId/:choice", (req, res) => {
     res.send("Thanks for voting!");
+  });
+
+  app.post("/api/surveys/webhooks", (req, res) => {
+    const p = new Path("/api/surveys/:surveyId/:choice");
+
+    const events = _.map(req.body, event => {
+      const pathname = new URL(event.url).pathname;
+      const match = p.test(pathname);
+      if (match) {
+        return {
+          email: event.email,
+          surveyId: match.surveyId,
+          choice: match.choice
+        };
+      }
+    });
+    const compactEvents = _.compact(events);
+    const uniqueEvents = _.uniqBy(compactEvents, "email", "surveyId");
+    _.each(uniqueEvents, event => {
+      Survey.updateOne(
+        {
+          _id: event.surveyId,
+          recipients: {
+            $elemMatch: { email: event.email, responded: false }
+          }
+        },
+        {
+          $inc: { [event.choice]: 1 },
+          $set: { "recipients.$.responded": true },
+          lastResponded: new Date()
+        }
+      ).exec();
+    });
+    res.send({});
   });
 };
